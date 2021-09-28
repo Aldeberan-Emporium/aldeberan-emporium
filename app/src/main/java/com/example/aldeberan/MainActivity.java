d89eea9b2bd1aadd026995131a89ffed954e6d5b
wilsonlkm98 <59793320+wilsonlkm98@users.noreply.github.com>
2021-09-28 17:22:53 +0800

    Using intent class to test other activity/fragment in MainActivity.java

--- app/src/main/java/com/example/aldeberan/MainActivity.java
+++ app/src/main/java/com/example/aldeberan/MainActivity.java
@@ -5,6 +5,7 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        //Code for test another activity pages.
+        Intent intent = new Intent(this, product_listing_detail_page.class);
+        startActivity(intent);
     }
 }
