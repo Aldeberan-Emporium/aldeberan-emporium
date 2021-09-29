4f793376d6cacf3f9e2533bba1210abe1d6ae99d
SC ONG <proscawards99@gmail.com>
2021-09-29 17:51:22 +0800

    Bug Fixed

--- app/src/main/java/com/example/aldeberan/AdminPanel.java
+++ app/src/main/java/com/example/aldeberan/AdminPanel.java
@@ -1,6 +1,10 @@
 package com.example.aldeberan;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
@@ -9,9 +13,11 @@ import android.widget.TextView;
 import com.example.aldeberan.models.ProductModel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class AdminPanel extends AppCompatActivity implements View.OnClickListener{
 
+    ProductModel pm = new ProductModel();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +26,22 @@ public class AdminPanel extends AppCompatActivity implements View.OnClickListene
         Button submitBtn = findViewById(R.id.submitBtn);
         submitBtn.setOnClickListener(this);
     }
+
+    public void pickImgFromGallery(){
+
+    }
+    /*
+    private boolean isPermissionGranted(){
+
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.R){
+            return Environment.isExternalStorageManager();
+        }
+        else{
+            int readExternalStorage = ContextCompat.checkSelfPermission(this.Manifest.permission.READ_EXTERNAL_STORAGE);
+            return readExternalStorage == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+    */
 
     @Override
     public void onClick(View view) {
@@ -30,6 +52,6 @@ public class AdminPanel extends AppCompatActivity implements View.OnClickListene
         String prodImg = "";
         Switch prodAvail = findViewById(R.id.prodAvail);
 
-        //addProduct(prodName.getEditableText(), prodSKU.getEditableText(), prodAvail, prodStock.getEditableText(), prodPrice.getEditableText(), prodImg);
+        //pm.addProduct(prodName.getEditableText(), prodSKU.getEditableText(), prodAvail, prodStock.getEditableText(), prodPrice.getEditableText(), prodImg);
     }
 }
