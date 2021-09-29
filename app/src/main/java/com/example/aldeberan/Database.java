d9115626ba5b4776edc597c952be28acd937247b
SC ONG <proscawards99@gmail.com>
2021-09-29 14:41:49 +0800

    Database Class Completed

--- app/src/main/java/com/example/aldeberan/Database.java
+++ app/src/main/java/com/example/aldeberan/Database.java
@@ -99,14 +99,200 @@ public class Database {
     }
 
     //Add quote
-    public void addQuote(String userID, double subtotal, double total, int prodStock, double prodPrice, String prodImg) throws JSONException, IOException {
+    public void addQuote(String userID, double subtotal, double total, String createdAt, String updatedAt, int quoteStatus) throws JSONException, IOException {
         JSONObject json = new JSONObject();
         json.put("action", "addQuote");
         json.put("user_id", StringEscapeUtils.escapeHtml3(userID));
         json.put("subtotal", subtotal);
-        json.put("product_stock", prodStock);
+        json.put("total", total);
+        json.put("created_at", createdAt);
+        json.put("updated_at", updatedAt);
+        json.put("quote_status", quoteStatus);
+        this.postData(json);
+    }
+
+    //Update quote
+    public void updateQuote(int quoteID, String userID, double subtotal, double total, String createdAt, String updatedAt, int quoteStatus) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "updateQuote");
+        json.put("quote_id", quoteID);
+        json.put("user_id", StringEscapeUtils.escapeHtml3(userID));
+        json.put("subtotal", subtotal);
+        json.put("total", total);
+        json.put("created_at", createdAt);
+        json.put("updated_at", updatedAt);
+        json.put("quote_status", quoteStatus);
+        this.postData(json);
+    }
+
+    //Delete quote
+    public void deleteQuote(int quoteID) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "deleteQuote");
+        json.put("quote_id", quoteID);
+        this.postData(json);
+    }
+
+    //Read quote by user id
+    public JsonObject readQuoteByUser(String userID) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "readQuoteByUser");
+        json.put("user_id", StringEscapeUtils.escapeHtml3(userID));
+        JsonObject data = new Gson().fromJson(this.postData(json), JsonObject.class);
+        return data;
+    }
+
+    //Add quote item
+    public void addQuoteItem(int quoteID, String prodName, String prodSKU, int prodQuantity, double prodPrice, String prodImg) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "addQuoteItem");
+        json.put("quote_id", quoteID);
+        json.put("product_name", StringEscapeUtils.escapeHtml3(prodName));
+        json.put("product_SKU", StringEscapeUtils.escapeHtml3(prodSKU));
+        json.put("product_quantity", prodQuantity);
         json.put("product_price", prodPrice);
-        json.put("product_img", StringEscapeUtils.escapeHtml3(prodImg));
+        json.put("product_img", prodImg);
+        this.postData(json);
+    }
+
+    //Update quote item
+    public void updateQuoteItem(int quoteItemID, int quoteID, String prodName, String prodSKU, int prodQuantity, double prodPrice, String prodImg) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "updateQuoteItem");
+        json.put("quote_item_id", quoteItemID);
+        json.put("quote_id", quoteID);
+        json.put("product_name", StringEscapeUtils.escapeHtml3(prodName));
+        json.put("product_SKU", StringEscapeUtils.escapeHtml3(prodSKU));
+        json.put("product_quantity", prodQuantity);
+        json.put("product_price", prodPrice);
+        json.put("product_img", prodImg);
+        this.postData(json);
+    }
+
+    //Delete quote item
+    public void deleteQuoteItem(int quoteItemID) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "deleteQuoteItem");
+        json.put("quote_item_id", quoteItemID);
+        this.postData(json);
+    }
+
+    //Add address
+    public void addAddress(String userID, String addRecipient, String addContact, String addLine1, String addLine2, String addCode, String addCity, String addState, String addCountry) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "addAddress");
+        json.put("user_id", StringEscapeUtils.escapeHtml3(userID));
+        json.put("address_recipient", StringEscapeUtils.escapeHtml3(addRecipient));
+        json.put("address_contact", StringEscapeUtils.escapeHtml3(addContact));
+        json.put("address_line1", StringEscapeUtils.escapeHtml3(addLine1));
+        json.put("address_line2", StringEscapeUtils.escapeHtml3(addLine2));
+        json.put("address_code", StringEscapeUtils.escapeHtml3(addCode));
+        json.put("address_city", StringEscapeUtils.escapeHtml3(addCity));
+        json.put("address_state", StringEscapeUtils.escapeHtml3(addState));
+        json.put("address_country", StringEscapeUtils.escapeHtml3(addCountry));
+        this.postData(json);
+    }
+
+    //Update address
+    public void updateAddress(int addID, String userID, String addRecipient, String addContact, String addLine1, String addLine2, String addCode, String addCity, String addState, String addCountry) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "updateAddress");
+        json.put("address_id", addID);
+        json.put("user_id", StringEscapeUtils.escapeHtml3(userID));
+        json.put("address_recipient", StringEscapeUtils.escapeHtml3(addRecipient));
+        json.put("address_contact", StringEscapeUtils.escapeHtml3(addContact));
+        json.put("address_line1", StringEscapeUtils.escapeHtml3(addLine1));
+        json.put("address_line2", StringEscapeUtils.escapeHtml3(addLine2));
+        json.put("address_code", StringEscapeUtils.escapeHtml3(addCode));
+        json.put("address_city", StringEscapeUtils.escapeHtml3(addCity));
+        json.put("address_state", StringEscapeUtils.escapeHtml3(addState));
+        json.put("address_country", StringEscapeUtils.escapeHtml3(addCountry));
+        this.postData(json);
+    }
+
+    //Delete address
+    public void deleteAddress(int addID) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "deleteAddress");
+        json.put("address_id", addID);
+        this.postData(json);
+    }
+
+    //Read address by user
+    public JsonObject readAddressByUser(String userID) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "readAddressByUser");
+        json.put("user_id", StringEscapeUtils.escapeHtml3(userID));
+        JsonObject data = new Gson().fromJson(this.postData(json), JsonObject.class);
+        return data;
+    }
+
+    //Add order
+    public void addOrder(String userID, String orderRef, String orderDate, double subtotal, double total, String orderStatus) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "addAddress");
+        json.put("user_id", StringEscapeUtils.escapeHtml3(userID));
+        json.put("order_reference", StringEscapeUtils.escapeHtml3(orderRef));
+        json.put("order_date", orderDate);
+        json.put("subtotal", subtotal);
+        json.put("total", total);
+        json.put("order_status", orderStatus);
+        this.postData(json);
+    }
+
+    //Admin read all orders
+    public JsonObject readOrderAll() throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "readOrderAll");
+        JsonObject data = new Gson().fromJson(this.postData(json), JsonObject.class);
+        return data;
+    }
+
+    //Read order by user id
+    public JsonObject readOrderByUser(String userID) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "readOrderByUser");
+        json.put("user_id", StringEscapeUtils.escapeHtml3(userID));
+        JsonObject data = new Gson().fromJson(this.postData(json), JsonObject.class);
+        return data;
+    }
+
+    //Add order item
+    public void addOrderItem(int orderID, String prodName, String prodSKU, int prodQuantity, double prodPrice, String prodImg) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "addOrderItem");
+        json.put("order_id", orderID);
+        json.put("product_name", StringEscapeUtils.escapeHtml3(prodName));
+        json.put("product_SKU", StringEscapeUtils.escapeHtml3(prodSKU));
+        json.put("product_quantity", prodQuantity);
+        json.put("product_price", prodPrice);
+        json.put("product_img", prodImg);
+        this.postData(json);
+    }
+
+    //Add order address
+    public void addOrderAddress(int orderID, String addRecipient, String addContact, String addLine1, String addLine2, String addCode, String addCity, String addState, String addCountry) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "addOrderAddress");
+        json.put("order_id", orderID);
+        json.put("address_recipient", StringEscapeUtils.escapeHtml3(addRecipient));
+        json.put("address_contact", StringEscapeUtils.escapeHtml3(addContact));
+        json.put("address_line1", StringEscapeUtils.escapeHtml3(addLine1));
+        json.put("address_line2", StringEscapeUtils.escapeHtml3(addLine2));
+        json.put("address_code", StringEscapeUtils.escapeHtml3(addCode));
+        json.put("address_city", StringEscapeUtils.escapeHtml3(addCity));
+        json.put("address_state", StringEscapeUtils.escapeHtml3(addState));
+        json.put("address_country", StringEscapeUtils.escapeHtml3(addCountry));
+        this.postData(json);
+    }
+
+    //Add order payment
+    public void addOrderPayment(int orderID, String payType, String payID) throws JSONException, IOException {
+        JSONObject json = new JSONObject();
+        json.put("action", "addOrderPayment");
+        json.put("order_id", orderID);
+        json.put("payment_type", StringEscapeUtils.escapeHtml3(payType));
+        json.put("payment_id", StringEscapeUtils.escapeHtml3(payID));
         this.postData(json);
     }
 }
