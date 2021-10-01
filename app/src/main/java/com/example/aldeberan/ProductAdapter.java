00e44405ed6b666c48f6ebb2f2d2a42b02af3bc0
SC ONG <proscawards99@gmail.com>
2021-10-01 18:27:19 +0800

    Implemented DataBinding to RecyclerView

--- app/src/main/java/com/example/aldeberan/ProductAdapter.java
+++ app/src/main/java/com/example/aldeberan/ProductAdapter.java
@@ -3,6 +3,7 @@ package com.example.aldeberan;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +13,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.aldeberan.databinding.ActivityMainBinding;
+import com.example.aldeberan.databinding.AdminPanelProductRowBinding;
 import com.example.aldeberan.models.ProductModel;
 import com.example.aldeberan.structures.Product;
 
@@ -22,82 +26,72 @@ import org.w3c.dom.Text;
 
 import java.util.List;
 
-public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> implements View.OnClickListener{
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder>{
 
     private Context mContext;
-    private List<Product> mData;
+    public List<Product> mData;
     ProductModel pm = new ProductModel();
-    public int currentPos;
+
 
     public ProductAdapter(Context mContext, List<Product> mData) {
         this.mContext = mContext;
         this.mData = mData;
     }
 
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()){
-            case R.id.updateBtn:
-                Toast.makeText(mContext, "update btn clicked, id: " +currentPos, Toast.LENGTH_SHORT).show();
-                //Show update screen
-                break;
-            case R.id.deleteBtn:
-                Toast.makeText(mContext, "delete btn clicked, id: " +currentPos, Toast.LENGTH_SHORT).show();
-                //Alert dialog to delete product
-                //pm.deleteProduct(mData.get(currentPos).getProdID());
-                break;
-        }
-    }
 
-    public static class ProductViewHolder extends RecyclerView.ViewHolder{
-        TextView prodID;
-        TextView prodName;
-        TextView prodSKU;
-        TextView prodAvail;
-        TextView prodStock;
-        TextView prodPrice;
-        ImageView prodImg;
+    public class ProductViewHolder extends RecyclerView.ViewHolder{
 
-        public ProductViewHolder(@NonNull View itemView) {
-            super(itemView);
+        AdminPanelProductRowBinding productRowBinding;
 
-            prodID = itemView.findViewById(R.id.prodIDLbl);
-            prodName = itemView.findViewById(R.id.prodNameLbl);
-            prodSKU = itemView.findViewById(R.id.prodSKULbl);
-            prodAvail = itemView.findViewById(R.id.prodAvailLbl);
-            prodStock = itemView.findViewById(R.id.prodStockLbl);
-            prodPrice = itemView.findViewById(R.id.prodPriceLbl);
-            prodImg = itemView.findViewById(R.id.prodImgView);
+        public ProductViewHolder(AdminPanelProductRowBinding productRowBinding) {
+            super(productRowBinding.getRoot());
+            this.productRowBinding = productRowBinding;
+
+            productRowBinding.updateBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("UPDATE", String.valueOf(mData.get(getAbsoluteAdapterPosition()).getProdID()));
+                    //Show update screen
+                }
+            });
+
+            productRowBinding.deleteBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("DELETE", String.valueOf(mData.get(getAbsoluteAdapterPosition()).getProdID()));
+                    //pm.deleteProduct(mData.get(currentPos).getProdID());
+                }
+            });
         }
     }
 
     @NonNull
     @Override
-    public ProductAdapter.ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view;
-        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-        view = layoutInflater.inflate(R.layout.admin_panel_product_row, parent, false);
+    public ProductAdapter.ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        AdminPanelProductRowBinding productRowBinding = DataBindingUtil.inflate(layoutInflater, R.layout.admin_panel_product_row, parent, false);
 
-        view.findViewById(R.id.updateBtn).setOnClickListener(this);
-        view.findViewById(R.id.deleteBtn).setOnClickListener(this);
-
-        return new ProductViewHolder(view);
+        return new ProductViewHolder(productRowBinding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
-        currentPos = position;
-        holder.prodName.setText(mData.get(position).getProdName());
-        holder.prodSKU.setText("SKU: " + mData.get(position).getProdSKU());
-        holder.prodID.setText("ID: (" + mData.get(position).getProdID() + ")");
+
+        final Product p = mData.get(position);
+        holder.productRowBinding.setProduct(p);
+        holder.productRowBinding.executePendingBindings();
+
+        holder.productRowBinding.prodNameLbl.setText(mData.get(position).getProdName());
+        holder.productRowBinding.prodSKULbl.setText("SKU: " + mData.get(position).getProdSKU());
+        holder.productRowBinding.prodIDLbl.setText("ID: (" + mData.get(position).getProdID() + ")");
 
         String prodAvail = mData.get(position).getProdAvail() ? "Active" : "Inactive";
 
-        holder.prodAvail.setText("Availability: " + prodAvail);
-        holder.prodStock.setText("Stock: " + mData.get(position).getProdStock());
-        holder.prodPrice.setText("Price: RM " + mData.get(position).getProdPrice());
+        holder.productRowBinding.prodAvailLbl.setText("Availability: " + prodAvail);
+        holder.productRowBinding.prodStockLbl.setText("Stock: " + mData.get(position).getProdStock());
+        holder.productRowBinding.prodPriceLbl.setText("Price: RM " + mData.get(position).getProdPrice());
 
-        Glide.with(mContext).load(mData.get(position).getProdImg()).override(450, 450).into(holder.prodImg);
+        Glide.with(mContext).load(mData.get(position).getProdImg()).override(450, 450).into(holder.productRowBinding.prodImgView);
     }
 
     @Override
