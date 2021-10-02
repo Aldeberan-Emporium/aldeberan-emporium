5c612ee488aef61178e34a19caeadf4b97d4ed50
wilsonlkm98 <59793320+wilsonlkm98@users.noreply.github.com>
2021-10-02 12:16:42 +0800

    Update homeproductFragment(for customer side) but got bug

--- app/src/main/java/com/example/aldeberan/homeProductFragment.java
+++ app/src/main/java/com/example/aldeberan/homeProductFragment.java
@@ -2,6 +2,7 @@ package com.example.aldeberan;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,12 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.aldeberan.models.ProductModel;
+import com.example.aldeberan.structures.Product;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,13 +25,66 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
-public class homeProductFragment extends Fragment {
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class homeProductFragment extends Fragment{
+
+    private View myFragmentView;
+    public List<Product> productList;
+    public RecyclerView recyclerView;
+    public ProductAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        myFragmentView = inflater.inflate(R.layout.fragment_home_product, container, false);
+        productList = new ArrayList<>();
+        recyclerView = myFragmentView.findViewById(R.id.recyclerView);
+
         /*
+        ConstructRecyclerView();
+        SwipeRefreshLayout pullToRefresh = myFragmentView.findViewById(R.id.pullToRefresh);
+
+        pullToRefresh.setOnRefreshListener(() -> {
+            ConstructRecyclerView();
+            adapter.notifyDataSetChanged();
+            pullToRefresh.setRefreshing(false);
+        });
+        */
+
+        return myFragmentView;
+    }
+
+    private void ConstructRecyclerView(){
+        ProductModel pm = new ProductModel();
+        try {
+            pm.readProductAll((response) -> {
+                productList = response;
+                PutDataIntoRecyclerView(response);
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void PutDataIntoRecyclerView(List<Product> productList){
+        adapter = new ProductAdapter(getContext(), productList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+        Log.i("PLOPE", String.valueOf(productList));
+    }
+
+    private void onProductDeleted(int index){
+        adapter.notifyItemRemoved(index);
+    }
+
+}
+
+/*
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference();
         DatabaseReference getImage = databaseReference.child("image");
@@ -53,10 +112,3 @@ public class homeProductFragment extends Fragment {
         });
 
          */
-
-
-
-
-        return inflater.inflate(R.layout.fragment_home_product, container, false);
-    }
-}
