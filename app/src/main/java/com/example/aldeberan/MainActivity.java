fe35b202dca333f188b13f1c7676ad4a79124633
KelvinDesktop <39939811+wkyg@users.noreply.github.com>
2021-10-02 19:28:43 +0800

    slash screen, remove title on spash

--- app/src/main/java/com/example/aldeberan/MainActivity.java
+++ app/src/main/java/com/example/aldeberan/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.aldeberan;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +12,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Code for test another activity pages.
-        Intent intent = new Intent(MainActivity.this, home_product.class);
-        startActivity(intent);
+        getSupportActionBar().hide();
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                //Code for test another activity pages.
+                finish();
+                Intent intent = new Intent(MainActivity.this, home_product.class);
+                startActivity(intent);
+            }
+
+        }, 3000L);
     }
 }
