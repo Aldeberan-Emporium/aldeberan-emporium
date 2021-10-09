ee9e4af3bd0a1ddb4a7663151ed94c0f38c24504
KelvinDesktop <39939811+wkyg@users.noreply.github.com>
2021-10-09 21:58:16 +0800

    vertical/ horizontal

--- app/src/main/java/com/example/aldeberan/Activity/Homepage.java
+++ app/src/main/java/com/example/aldeberan/Activity/Homepage.java
@@ -8,14 +8,10 @@ import com.example.aldeberan.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Homepage extends AppCompatActivity {
-
-    BottomNavigationView botNavView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-
-        botNavView = findViewById(R.id.botNavView);
 
     }
 }
