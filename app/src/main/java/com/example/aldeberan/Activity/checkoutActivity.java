702b72b8104a7660b97e4ad8612ee81bc1470104
wilsonlkm98 <59793320+wilsonlkm98@users.noreply.github.com>
2021-10-23 04:07:15 +0800

    Last, put name on action bar, goodnight :D

--- app/src/main/java/com/example/aldeberan/Activity/checkoutActivity.java
+++ app/src/main/java/com/example/aldeberan/Activity/checkoutActivity.java
@@ -42,6 +42,7 @@ public class checkoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+        getSupportActionBar().setTitle("Items Checkout");
 
         os = new OrderStorage(this);
         TextView price = findViewById(R.id.price);
