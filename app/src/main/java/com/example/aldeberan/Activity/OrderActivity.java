f204aaed4de92bb900fcb1f3d5ec98ec442964f9
KelvinDesktop <39939811+wkyg@users.noreply.github.com>
2021-10-30 16:19:01 +0800

    order hist ui change

--- app/src/main/java/com/example/aldeberan/Activity/OrderActivity.java
+++ app/src/main/java/com/example/aldeberan/Activity/OrderActivity.java
@@ -1,11 +1,13 @@
 package com.example.aldeberan.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -36,6 +38,7 @@ public class OrderActivity extends AppCompatActivity implements Serializable {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_order_history);
         recyclerView = findViewById(R.id.orderRecyclerView);
 
